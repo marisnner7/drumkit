77483c5f1cec77095b7c807b1ec11ca7a0189d20
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-07 23:08:50 -0300

    setting transition

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
+
 window.addEventListener('keydown', (e)=>{
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if(!audio) return
   audio.currentTime = 0;
   audio.play();
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 });
